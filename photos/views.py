47567608760d311c5ee21296de94f4f06bfbec1b
John Kinya <kingjohnkn@gmail.com>
2023-06-13 02:02:31 +0300

    finalize app

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Photo
 
 # Create your views here.
 
 
 def gallery(request):
+    category = request.GET.get("category")
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {"categories": categories, "photos": photos}
     return render(request, "photos/gallery.html", context)
 
@@ -17,4 +22,28 @@ def view_photo(request, pk):
 
 
 def add_photo(request):
-    return render(request, "photos/add.html")
+    categories = Category.objects.all()
+
+    if request.method == "POST":
+        data = request.POST
+        image = request.FILES.get("image")
+
+        if data["category"] != "none":
+            category = Category.objects.get(id=data["category"])
+        elif data["category_new"] != "":
+            category, created = Category.objects.get_or_create(
+                name=data["category_new"]
+            )
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category=category,
+            description=data["description"],
+            image=image,
+        )
+
+        return redirect("gallery")
+
+    context = {"categories": categories}
+    return render(request, "photos/add.html", context)
